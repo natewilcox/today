2f2c67ea9b0cd1315685d750149a5a64db07beb9
Nathan Wilcox <natewilcox@gmail.com>
2024-11-02 21:09:38 -0400

    add crappy version

--- src/main/java/io/natewilcox/App.java
+++ src/main/java/io/natewilcox/App.java
@@ -1,8 +1,20 @@
 package io.natewilcox;
 
+import java.io.IOException;
+
 public class App {
     public static void main(String[] args) {
 
         Command cmd = new Command(args);
+
+        try {
+            cmd.execute();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/io/natewilcox/Command.java
+++ src/main/java/io/natewilcox/Command.java
@@ -1,8 +1,45 @@
 package io.natewilcox;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+
 public class Command {
+
+    private HttpClient client;
 
     public Command(String[] args) {
 
+        this.client = HttpClient.newHttpClient();
     }
-} 
+
+    public void execute() throws IOException, InterruptedException {
+
+        LocalDate currentDate = LocalDate.now();
+
+        String type = "deaths";
+        int mm = currentDate.getMonthValue();
+        int dd = currentDate.getDayOfMonth();
+
+        String url = String.format("https://en.wikipedia.org/api/rest_v1/feed/onthisday/%s/%d/%d", type, mm, dd);
+
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
+        HttpResponse response = this.client.send(request, BodyHandlers.ofString());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree((String)response.body());
+        
+        JsonNode items = jsonNode.get(type);
+
+        for(JsonNode item : items) {
+            System.out.println(item.get("text").asText());
+        }
+    }
+}
